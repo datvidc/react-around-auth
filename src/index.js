d4a6315dc9105e5937116a293208bf0ec32f22cc
David Christoffersen <d@vidc.dk>
2020-10-20 23:13:06 -0400

    WHAAAAAT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/react-around-auth'>
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
