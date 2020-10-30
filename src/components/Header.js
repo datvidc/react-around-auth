e4acc45f0c61fdfdb42079ac874c630ef97b80e1
David Christoffersen <d@vidc.dk>
2020-10-30 10:08:13 -0400

    If props.email. makes sense

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Vector from '../images/Vector.svg';
 
-function Header() {
+function Header(props) {
   return (
   <header className="header">
     <img className="header__logo" src={Vector} alt="Around the US" />
-   <div className="header__loginMenu">> 
+   <div className="header__loginMenu">
+    {if (props.userEmail) { 
     <p className='header__email'> </p>
+    }}
     <button className="header__signBtn"></button>
   </div> 
   </header>
