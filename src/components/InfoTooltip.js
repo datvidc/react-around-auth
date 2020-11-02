2612c34112da2b9575d67e3cee0cf960711ea16e
David Christoffersen <d@vidc.dk>
2020-11-02 10:39:19 -0500

    isInfoToolTipOpen

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -5,7 +5,7 @@ function InfoToolTip(props) {
 
     return (
 
-        <div className={`popup popup_type_${props.name} ${isOpenClass}`}>
+        <div className={`infoToolTip popup_type_${props.name} ${isOpenClass}`}>
         <div className="popup__container">
           <button onClick={props.closeItAll} type="submit" className="popup__close"> </button>
           <form className="popup__edit-form" onSubmit={props.onSubmit}>
