bb640b03e9b4d835497314da8799ea5fb08e0287
David Christoffersen <d@vidc.dk>
2020-11-19 11:15:46 -0500

    tooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -8,23 +8,24 @@ import PopupWithForm from './PopupWithForm';
 function InfoToolTip(props) {
     const message = props.success ? "Success! You have now been registered." : "Oops, something went wrong! Please try again.";
 
-    const icon = props.success ? successIcon : failureIcon ;
+    const icon = props.success ? successIcon : failureIcon;
 
     return (
 
-    <>
-        <PopupWithForm noButton="true"  name="infoToolTip" isOpen={props.isOpen} closeItAll={props.onClose} >
-            <img 
-                className="popup__successIcon"
-                src={icon}
-                alt="CheckMark"
-            />
-            <h2> {message} </h2>
-        
-        </PopupWithForm>
+        <>
+            <PopupWithForm noButton="true" name="infoToolTip" isOpen={props.isOpen} closeItAll={props.onClose} >
+                <div className="popup__infoToolTip">
+                    <img
+                        className="popup__successIcon"
+                        src={icon}
+                        alt="CheckMark"
+                    />
+                    <h2 className="popup__message" > {message} </h2>
+                </div>
+            </PopupWithForm>
 
-    </>
-    
+        </>
+
     )
 
 }
