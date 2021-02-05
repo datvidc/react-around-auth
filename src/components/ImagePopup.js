2c1235b2ed9d2383bea51a6d47e499894a466964
David Christoffersen <d@vidc.dk>
2021-02-05 18:22:12 -0500

    Button type changed from submit to button

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -7,7 +7,7 @@ function ImagePopup(props) {
 
     <div className={`popup popup__img ${isOpenClass}`} >
       <div className="popup__card popup_image">
-        <button type="submit" onClick={props.onClose} className="popup__close popup__closeimg"> </button>
+        <button type="button" onClick={props.onClose} className="popup__close popup__closeimg"> </button>
         <img src={props.card && props.card.link} alt={props.card.name} className="popup__image popImg" />
         <p className="popup__imgtext">{props.card.name} </p>
       </div>
